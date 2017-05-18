59107ee1e0e9e5b501f662e94c1b49bd3f2b4cfa
Hysperr <Hysperr@users.noreply.github.com>
2017-05-18 19:58:47 -0400

    Queue

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "queue.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "default capacity: " << queue<int>::capacity() << std::endl;
+
+    queue<int> q;
+    for (int i = 0; i < 10; i++) q.insert(i);
+    std::cout << "10 customers in line..." << std::endl;
+    q.print_queue();
+
+    std::cout << "customer has finished purchase..." << std::endl;
+    q.remove();
+    q.print_queue();
+    std::cout << "current space left: " << q.current_space_left() << std::endl;
+
+
+    std::cout << "another customer has finished purchase..." << std::endl;
+    q.remove();
+    q.print_queue();
+    std::cout << "current space left: " << q.current_space_left() << std::endl;
+    std::cout << "queue size is now: " << q.size() << std::endl;
+
+    std::cout << "removing elements from queue...\n";
+    int qsize = (int) q.size();
+    for (int i = 0; i < qsize; i++) {
+        std::cout << q.remove() << std::endl;
+    }
+    assert(q.size() == 0);
+
+    std::cout << "queue size: " << q.size() << std::endl;
+    q.print_queue();
+
     return 0;
 }
--- Queue/queue.h
+++ Queue/queue.h
@@ -2,15 +2,16 @@
 #ifndef QUEUE_QUEUE_H
 #define QUEUE_QUEUE_H
 
+#include <cstddef>
+
 template<typename T>
 class queue {
 
-    const static int ARRAY_SIZE = 5;
+    typedef size_t size_type;
+    const static int ARRAY_SIZE = 30;
 
 public:
     queue();
-
-    ~queue();
 
     void insert(T element);
 
@@ -22,11 +23,17 @@ public:
 
     void print_queue() const;
 
+    size_type size() const { return used; }
+
+    static const int capacity() { return ARRAY_SIZE; }
+
+    size_type current_space_left() const { return ARRAY_SIZE - used; }
+
 private:
     T data[ARRAY_SIZE];
     int front;  // where next elem removed
     int back;   // where next elem inserted
-
+    size_type used;
 };
 
 #include "queue.template"
