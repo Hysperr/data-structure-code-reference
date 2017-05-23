dc46721c6046dae22840c0ed6f2ab85075bda37f
Hysperr <Hysperr@users.noreply.github.com>
2017-05-22 23:50:57 -0400

    Stack Pair

---
+++ Stack Pair/main.cpp
@@ -0,0 +1,51 @@
+
+#include "stack_pair.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    stack_pair<int> sp;
+
+    cout << "stack_pair created with ends front end A & back end B." << endl;
+    cout << "Is A empty? " << sp.is_empty_a() << endl;
+    cout << "Is B empty? " << sp.is_empty_b() << endl;
+    cout << "print stack_pair: " << endl;
+    sp.print_stack_pair();
+    cout << '\n';
+
+
+    cout << "Capacity: " << stack_pair<int>::CAPACITY << endl;
+    cout << "Push 15 items into A..." << endl;
+    for (int i = 1; i <= 15; i++) {
+        sp.push_a(i);
+    }
+    cout << "A size: " << sp.size_a() << endl;
+    cout << "print stack_pair: " << endl;
+    sp.print_stack_pair();
+    std::cout << '\n';
+
+
+    cout << "Push 18 items into B to fill stack_pair..." << endl;
+    for (int i = 30; i > 15; i--) {
+        sp.push_b(i);
+    }
+    cout << "B size: " << sp.size_b() << endl;
+    cout << "print stack_pair: " << endl;
+    sp.print_stack_pair();
+    std::cout << '\n';
+
+
+    cout << "A full? " << sp.is_full_a() << endl;
+    cout << "B full? " << sp.is_full_b() << endl;
+
+    cout << "Popping from A returns " << sp.pop_a() << endl;
+    cout << "A size: " << sp.size_a() << endl;
+
+    cout << "print stack_pair: " << endl;
+    sp.print_stack_pair();
+    std::cout << '\n';
+
+
+    return 0;
+}
---
+++ Stack Pair/stack_pair.h
@@ -0,0 +1,43 @@
+
+#ifndef STACK_PAIR_STACK_PAIR_H
+#define STACK_PAIR_STACK_PAIR_H
+
+#include <cstddef>
+
+template<class T>
+class stack_pair {
+
+public:
+
+    typedef std::size_t size_type;
+    static const size_type CAPACITY = 30;
+
+    stack_pair();
+
+    T pop_a();
+    T pop_b();
+
+    void push_a(T item);
+    void push_b(T item);
+
+    size_type size_a() const;
+    size_type size_b() const;
+
+    bool is_empty_a() const;
+    bool is_empty_b() const;
+
+    bool is_full_a() const;
+    bool is_full_b() const;
+
+    void print_stack_pair() const;
+
+private:
+    T data[CAPACITY];
+    size_type top_a;
+    size_type top_b;
+};
+
+#include "stack_pair.template"
+
+
+#endif //STACK_PAIR_STACK_PAIR_H
