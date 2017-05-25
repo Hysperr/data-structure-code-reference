17be6b70fffd1e36ae30612e00a73e7fe236f075
Hysperr <Hysperr@users.noreply.github.com>
2017-05-25 09:56:11 -0400

    update documentation

--- Binary Tree/binary_tree.h
+++ Binary Tree/binary_tree.h
@@ -1,77 +1,77 @@
 
 /**
-PROVIDES: A template class for a node in a binary tree and functions for
+PROVIDES: A template typename for a node in a binary tree and functions for
 manipulating binary trees. The template parameter is the type of data in
 each node.
 
-TYPEDEF for the binary_tree_node<Item> template class:
+TYPEDEF for the binary_tree<T> template typename:
 Each node of the tree contains a piece of data and pointers to its
-children. The type of the data (binary_tree_node<Item>::value_type) is
+children. The type of the data (binary_tree<T>::value_type) is
 the Item type from the template parameter. The type may be any of the C++
-built-in types (int, char, etc.), or a class with a default constructor,
+built-in types (int, char, etc.), or a typename with a default constructor,
 and an assignment operator.
 
-CONSTRUCTOR for the binary_tree_node<Item> class:
-binary_tree_node(
+CONSTRUCTOR for the binary_tree<T> typename:
+binary_tree(
 const item& init_data = Item( ),
-binary_tree_node<Item>* init_left = NULL,
-binary_tree_node<Item>* init_right = NULL
+binary_tree<T>* init_left = NULL,
+binary_tree<T>* init_right = NULL
 )
 Postcondition: The new node has its data equal to init_data,
 and it's child pointers equal to init_left and init_right.
 
-MEMBER FUNCTIONS for the binary_tree_node<Item> class:
+MEMBER FUNCTIONS for the binary_tree<T> typename:
 const item& data( ) const      <----- const version
 and
 Item& data( )                  <----- non-const version
 Postcondition: The return value is a reference to the data from
-this binary_tree_node.
+this binary_tree.
 
-const binary_tree_node* left( ) const  <----- const version
+const binary_tree* left( ) const  <----- const version
 and
-binary_tree_node* left( )              <----- non-const version
+binary_tree* left( )              <----- non-const version
 and
-const binary_tree_node* right( ) const <----- const version
+const binary_tree* right( ) const <----- const version
 and
-binary_tree_node* right( )             <----- non-const version
+binary_tree* right( )             <----- non-const version
 Postcondition: The return value is a pointer to the left or right child
 (which will be NULL if there is no child).
 
 void set_data(const Item& new_data)
-Postcondition: The binary_tree_node now contains the specified new data.
+Postcondition: The binary_tree now contains the specified new data.
 
-void set_left(binary_tree_node* new_link)
+void set_left(binary_tree* new_link)
 and
-void set_right(binary_tree_node* new_link)
-Postcondition: The binary_tree_node now contains the specified new link
+void set_right(binary_tree* new_link)
+Postcondition: The binary_tree now contains the specified new link
 to a child.
 
 bool is_leaf( )
 Postcondition: The return value is true if the node is a leaf;
 otherwise the return value is false.
 
-NON-MEMBER FUNCTIONS to maniplulate binary tree nodes:
-tempate <class Process, class BTNode>
-void inorder(Process f, BTNode* node_ptr)
+NON-MEMBER FUNCTIONS to manipulate binary tree nodes:
+template <typename Process, typename BinTree>
+void inorder(Process f, BinTree* node_ptr)
 Precondition: node_ptr is a pointer to a node in a binary tree (or
 node_ptr may be NULL to indicate the empty tree).
 Postcondition: If node_ptr is non-NULL, then the function f has been
 applied to the contents of *node_ptr and all of its descendants, using
 an in-order traversal.
-Note: BTNode may be a binary_tree_node or a const binary tree node.
+Note: BinTree may be a binary_tree or a const binary tree node.
 Process is the type of a function f that may be called with a single
 Item argument (using the Item type from the node).
 
-tempate <class Process, class BTNode>
-void postorder(Process f, BTNode* node_ptr)
+template <typename Process, typename BinTree>
+void postorder(Process f, BinTree* node_ptr)
 Same as the in-order function, except with a post-order traversal.
 
-tempate <class Process, class BTNode>
-void preorder(Process f, BTNode* node_ptr)
+template <typename Process, typename BinTree>
+void preorder(Process f, BinTree* node_ptr)
 Same as the in-order function, except with a pre-order traversal.
 
-template <class Item, class SizeType>
-void print(const binary_tree_node<Item>* node_ptr, SizeType depth)
+template <typename Item, typename SizeType>
+void print(const binary_tree<T>* node_ptr, SizeType depth)
 Precondition: node_ptr is a pointer to a node in a binary tree (or
 node_ptr may be NULL to indicate the empty tree). If the pointer is
 not NULL, then depth is the depth of the node pointed to by node_ptr.
@@ -80,22 +80,22 @@ and all its descendants have been written to cout with the << operator,
 using a backward in-order traversal. Each node is indented four times
 its depth.
 
-template <class Item>
-void tree_clear(binary_tree_node<Item>*& root_ptr)
+template <typename Item>
+void tree_clear(binary_tree<T>*& root_ptr)
 Precondition: root_ptr is the root pointer of a binary tree (which may
 be NULL for the empty tree).
 Postcondition: All nodes at the root or below have been returned to the
 heap, and root_ptr has been set to NULL.
 
-template <class Item>
-binary_tree_node<Item>* tree_copy(const binary_tree_node<Item>* root_ptr)
+template <typename Item>
+binary_tree<T>* tree_copy(const binary_tree<T>* root_ptr)
 Precondition: root_ptr is the root pointer of a binary tree (which may
 be NULL for the empty tree).
 Postcondition: A copy of the binary tree has been made, and the return
 value is a pointer to the root of this copy.
 
-template <class Item>
-size_t tree_size(const binary_tree_node<Item>* node_ptr)
+template <typename Item>
+size_t tree_size(const binary_tree<T>* node_ptr)
 Precondition: node_ptr is a pointer to a node in a binary tree (or
 node_ptr may be NULL to indicate the empty tree).
 Postcondition: The return value is the number of nodes in the tree.
